77bcc29e3f49fd825be555fb926b56b7667dc5af
jayteealao <jayteealao@gmail.com>
2021-08-21 17:22:10 +0100

    WIP implementation using aiohttp

--- NaijaBet_Api/bookmakers/bet9ja.py
+++ NaijaBet_Api/bookmakers/bet9ja.py
@@ -2,6 +2,9 @@ from NaijaBet_Api.utils.normalizer import bet9ja_match_normalizer
 import requests
 from NaijaBet_Api.id import Betid
 from NaijaBet_Api.utils import jsonpaths
+import asyncio
+import aiohttp
+
 
 """
 [summary]
@@ -19,14 +22,19 @@ class Bet9ja:
         session: holds a requests session object for the class as a static variable.
     """
 
-    session = requests.Session()
-    session.get("https://sports.bet9ja.com")
+    session = requests
+    async_session = aiohttp
 
-    def __init__(self) -> None:
+    def __init__(self, session_type='blocking') -> None:
         """
         Inits the class
         """
         self.site = "bet9ja"
+        if session_type == 'blocking':
+            self.session = Bet9ja.session.session()
+            self.session.get("https://sports.bet9ja.com/")
+        else:
+            self.launched = False
 
     def get_nations(self, nation: str):
 
@@ -51,6 +59,8 @@ class Bet9ja:
         Returns:
             [type]: [description]
         """
+        self.session = Bet9ja.session.session()
+        self.session.get("https://sports.bet9ja.com/")
         try:
             res = Bet9ja.session.get(url=league.to_endpoint(self.site))
             # print(res.status_code)
@@ -62,6 +72,36 @@ class Bet9ja:
             self.data = bet9ja_match_normalizer(jsonpaths.bet9ja_validator(self.rawdata))
             # self.data = jsonpaths.bet9ja_validator(self.rawdata)
             return self.data
+    
+    async def launch_async(self):
+        self.session = Bet9ja.async_session.ClientSession(connector_owner=False)
+        await self.session.get("https://sports.bet9ja.com/")
+
+    async def async_get_league(self, league: Betid = Betid.PREMIERLEAGUE, async_session: aiohttp.ClientSession = None):
+        """
+        Provides access to available league level odds for unplayed matches
+
+        Returns:
+            [type]: [description]
+        """
+        if not self.launched:
+            await self.launch_async()
+            self.launched = True
+        if not async_session:
+            async_session = self.session
+        async with async_session as session:
+            try:
+                res = await session.get(url=league.to_endpoint(self.site))
+                # print(res.status_code)
+            except Exception as e:
+                print(e)
+                return {}
+            else:
+                self.rawdata = await res.json()
+            if self.rawdata["R"] == "OK":
+                self.data = bet9ja_match_normalizer(jsonpaths.bet9ja_validator(self.rawdata))
+                # self.data = jsonpaths.bet9ja_validator(self.rawdata)
+                return self.data
 
     def get_all(self):
         """
@@ -74,3 +114,23 @@ class Bet9ja:
         for league in Betid:
             self.data += self.get_league(league)
         return self.data
+
+    async def async_get_all(self):
+        """
+        provides odds for all 1x2 and doublechance markets for all implemented leagues
+
+        Returns:
+            Sequence[Mapping[str, str]]: A lis
+        """
+        if not self.launched:
+            await self.launch_async()
+            self.launched = True
+        self.data = []
+        tasks = []
+        async with self.session as session:
+            for league in Betid:
+                tasks.append(asyncio.ensure_future(self.async_get_league(league, session)))
+            work = await asyncio.gather(*tasks)
+            for league in work:
+                self.data += league
+            return self.data
